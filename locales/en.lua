d6098c92baf16785b79351a0bb48c2c67092b26b
fifly <107129715+Fifly1@users.noreply.github.com>
2024-06-01 15:14:50 -0700

    Add files via upload

--- locales/en.lua
+++ locales/en.lua
@@ -1,1 +1,18 @@
+local Translations = {
+    error = {
+        not_in_range = 'Too far from the job center'
+    },
+    info = {
+        new_job_app = 'Your application was sent to the boss of %{job}',
+        blip_text = 'Job Center',
+        job_center_menu = '[E] Open Job Center',
+        new_job = 'Congratulations with your new job! (%{job})',
+        target_label = "Open Job Center",
+        cooldown = 'You must wait %{time} hours before applying again.',
+    },
+}
 
+Lang = Lang or Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
